06325ee35073836e9a6f73ada01851eb6d3ef922
Mike Rensel <rensel.michael@gmail.com>
2018-02-07 13:57:34 -0500

    stuff

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,11 +16,11 @@ namespace GradeBook.GradeBooks
         throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
       var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-      var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList;
+      var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-      if(grades[threshold - 1] <= averageGrade)
+      if (grades[threshold - 1] <= averageGrade)
         return 'A';
-      else if (grades[(threshold*2)-1] <= averageGrade)
+      else if (grades[(threshold*2) - 1] <= averageGrade)
         return 'B';
 
       return base.GetLetterGrade(averageGrade);
