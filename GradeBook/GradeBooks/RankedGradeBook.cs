c80ec2a8bcd65114adddbadd2f1d65b92d66d8de
Mike Rensel <mikemike26@users.noreply.github.com>
2018-02-07 15:29:04 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,15 +31,15 @@ namespace GradeBook.GradeBooks
         return 'F';
     }
 
-//     public override void CalculateStatistics()
-//     {
-//       if(Students.Count <5)
-//       {
-//         Console.WriteLine("Ranked grading requires at least 5 students.");
-//         return;
-//       }
+    public override void CalculateStatistics()
+    {
+      if(Students.Count <5)
+      {
+        Console.WriteLine("Ranked grading requires at least 5 students.");
+        return;
+      }
       
-//       base.CalculateStatistics();
-//     }
+      base.CalculateStatistics();
+    }
   }
 }
