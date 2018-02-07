e02ae1a72207ec2816b406bd3eda6a073cb3b181
Mike Rensel <rensel.michael@gmail.com>
2018-02-07 15:42:06 -0500

    stuff

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,8 +38,19 @@ namespace GradeBook.GradeBooks
         Console.WriteLine("Ranked grading requires at least 5 students.");
         return;
       }
-      
+
       base.CalculateStatistics();
+    }
+
+    public override void CalculateStudentStatistics(string name)
+    {
+      if(Students.Count <5)
+      {
+        Console.WriteLine("Ranked grading requires at least 5 students.");
+        return;
+      }
+
+      base.CalculateStudentStatistics(name)
     }
   }
 }
